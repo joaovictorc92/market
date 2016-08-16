bb86f0ec06e565c21f890c64af602939358115d7
joaovictorc92 <joaovictorc92@gmail.com>
2016-08-15 22:56:45 -0300

    mapeamento empresa e tipo

--- src/main/java/com/market/model/Empresa.java
+++ src/main/java/com/market/model/Empresa.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -13,23 +15,11 @@ public class Empresa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idempresa;
 	private String nome;
-	private String cnpj;
-	private Integer tipo;
+	private String cnpj;	
+	@ManyToOne
+	@JoinColumn(name="tipo")
+	private TipoEmpresa tipo;
 	private String endereco;
-
-	public Empresa(String nome, String cnpj, Integer tipo, String endereco) {
-		this.nome = nome;
-		this.cnpj = cnpj;
-		this.tipo = tipo;
-		this.endereco = endereco;
-	}
-	
-	public Empresa(){
-		this.nome = "";
-		this.cnpj = "";
-		this.tipo = 0;
-		this.endereco = "";
-	}
 	
 	public Integer getIdempresa() {
 		return idempresa;
@@ -55,14 +45,6 @@ public class Empresa {
 		this.cnpj = cnpj;
 	}
 
-	public Integer getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(Integer tipo) {
-		this.tipo = tipo;
-	}
-
 	public String getEndereco() {
 		return endereco;
 	}
@@ -70,5 +52,15 @@ public class Empresa {
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
+
+	public TipoEmpresa getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoEmpresa tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 
 }
